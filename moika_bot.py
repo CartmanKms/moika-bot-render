cc179af78ca03d94de01ed3c588ff8447f2acdf3
Михаил Смотров <mihailsmotrov@MacMini-M4.local>
2025-04-16 02:03:20 +1000

    Исправил синтаксическую ошибку в токене

--- moika_bot.py
+++ moika_bot.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")nfr  # ← твой токен
+BOT_TOKEN = os.environ.get("TELEGRAM_TOKEN")  # ← твой токен
 GROUP_CHAT_ID = -4680007622  # ← ID твоей группы
 
 user_message_map = {}
